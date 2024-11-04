7c9c954b1b8016cd2dd74d124c09d15c0c0f7148
Md Emdadullah <86929969+developeremdad@users.noreply.github.com>
2024-11-04 16:19:37 +0600

    services connect to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,13 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
   @Get() // Get /users
   findAll() {
     return ['User 1', 'User 2', 'User 3'];
@@ -20,32 +23,41 @@ export class UsersController {
     return ['Nested URL 1', 'Nested URL 2', 'Nested URL 3'];
   }
 
+  @Get('with-query')
+  getAllUserWithQuery(@Query('role') role: 'admin' | 'user') {
+    return this.userService.findAllWithRole(role);
+  }
+
   @Get(':id') // Get /users/:id
   findOne(@Param('id') id: number) {
-    return `User with ID: ${id}`;
+    // return `User with ID: ${id}`;
+    return this.userService.getUserById(id);
   }
 
   @Post('create')
   createUser(@Body() user: any) {
-    return {
-      message: 'User created successfully',
-      data: user,
-    };
+    // return {
+    //   message: 'User created successfully',
+    //   data: user,
+    // };
+    return this.userService.createUser(user);
   }
 
   @Patch()
   updateUser(@Param('id') id: number, @Body() user: any) {
-    return {
-      message: 'User updated successfully',
-      data: { id, ...user },
-    };
+    // return {
+    //   message: 'User updated successfully',
+    //   data: { id, ...user },
+    // };
+    return this.userService.updateUser(id, user);
   }
 
   @Delete(':id')
   deleteUser(@Param('id') id: number) {
-    return {
-      message: 'User deleted successfully',
-      data: { id },
-    };
+    // return {
+    //   message: 'User deleted successfully',
+    //   data: { id },
+    // };
+    return this.userService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
 
+export interface User {
+  id: number;
+  name: string;
+  role: string;
+  email: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: 1, role: 'user', name: 'Alice Johnson', email: 'alice@example.com' },
+    { id: 2, role: 'user', name: 'Bob Smith', email: 'bob@example.com' },
+    {
+      id: 3,
+      role: 'admin',
+      name: 'Charlie Brown',
+      email: 'charlie@example.com',
+    },
+  ];
+
+  findAllWithRole(role?: 'admin' | 'user') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  createUser(user: User) {
+    this.users.push(user);
+    return user;
+  }
+
+  getUserById(id: number): User {
+    return this.users.find((user) => user.id === id);
+  }
+
+  updateUser(id: number, user: User) {
+    const existingUserIndex = this.users.findIndex((u) => u.id === id);
+    if (existingUserIndex !== -1) {
+      this.users[existingUserIndex] = user;
+      return this.users[existingUserIndex];
+    }
+    return false;
+  }
+
+  deleteUser(id: number) {
+    const existingUserIndex = this.users.findIndex((u) => u.id === id);
+    if (existingUserIndex !== -1) {
+      this.users.splice(existingUserIndex, 1);
+      return true;
+    }
+    return false;
+  }
+}
